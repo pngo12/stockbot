79c3e23a587f62c099874adae49511155cbb9b54
pngo12 <pngo12@gmail.com>
2021-01-26 08:48:29 -0800

    Added some more info

--- iex/iex.js
+++ iex/iex.js
@@ -7,18 +7,24 @@ const getTickerInfo = async ticker => {
     let response = await fetch(
       `https://cloud.iexapis.com/stable/stock/${ticker}/quote?token=${APIToken}`
     );
-    // const jsonResponse = await response.json().then(data => data);
-    let jsonResponse = await response.json();
-    return jsonResponse;
-    // await fetchData(ticker)
+    const isError = await Promise.resolve(handleErrors(response.ok));
+    if (isError) {
+      // Do something
+    } else {
+      let jsonResponse = await response.json();
+      return jsonResponse;
+    }
   }
 };
 
-// const fetchData = async ticker => {
-//     let response = await fetch(`https://cloud.iexapis.com/stable/stock/${ticker}/quote?token=${APIToken}`);
-//     // const jsonResponse = await response.json().then(data => data);
-//     let jsonResponse = await response.json();
-//     return jsonResponse;
-// }
+const handleErrors = async response => {
+  // ACTUALLY handle errors here
+  // vs truthy falsy returns
+  if (!response) {
+    return true;
+  } else {
+    return false;
+  } 
+}
 
 exports.getTickerInfo = getTickerInfo;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
@@ -8,19 +8,60 @@ const util = require("./util/index");
 
 bot.login(TOKEN);
 
-bot.on('message', async msg => {
+bot.on("message", async (msg) => {
   if (msg.content[0].includes("$")) {
     const ticker = util.prepString(msg.content);
     const response = await tickerHandler(ticker);
-    msg.reply(`The Symbol you requested is: ${response.symbol}, Last price is ${response.latestPrice}`);
-  } else { // Error handling
 
+    const {
+      latestPrice,
+      change,
+      changePercent,
+      week52High,
+      week52Low,
+      extendedPrice,
+    } = response;
+
+    msg.reply(
+      `
+    ***${symbol}***
+    Latest Price: ${latestPrice}
+    Change Amount: ${change}
+    Change Percent: ${changePercent * 100}
+    52wk High: ${week52High}
+    52wk Low: ${week52Low}
+    After Hours Price: ${extendedPrice || "Not Available"}
+    `
+    );
   }
 });
 
-const tickerHandler = async symbol => {
+// Uncomment to pull quote in terminal
+// bot.on('ready', async () => {
+
+//   const response = await tickerHandler('aapl');
+
+//   const {
+//     latestPrice,
+//     change,
+//     changePercent,
+//     week52High,
+//     week52Low,
+//     extendedPrice,
+//   } = response;
+
+//     console.log(`
+//     ***${symbol}***
+//     Latest Price: ${latestPrice}
+//     Change Amount: ${change || null}
+//     Change Percent: ${changePercent * 100}
+//     52wk High: ${week52High}
+//     52wk Low: ${week52Low}
+//     After Hours Price: ${extendedPrice || "Not Available"}
+//     `);
+// });
+
+const tickerHandler = async (symbol) => {
   const getTickerInfo = await iex.getTickerInfo(symbol);
   return getTickerInfo;
 };
-
-
