90a5386073cd319f3cd9804bf05958f4dac6441d
pngo12 <pngo12@gmail.com>
2021-02-25 20:05:09 -0800

    Cron Changes

--- cron/cron.js
+++ cron/cron.js
@@ -1,24 +1,25 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+bot.login(TOKEN);
+
 const cron = require("node-cron");
-// const TOKEN = process.env.TOKEN;
-// bot.login(TOKEN);
-
 const util = require("../util/index");
-
 
 const scheduleSPYSummaryOpen = async () => {
   cron.schedule("30 6 * * Monday-Friday", async () => {
-    const response = await util.tickerHandler("$SPY");
+    const response = await util.tickerHandler("SPY");
     const { latestPrice, previousClose, ytdChange } = response;
 
-    bot.channels.cache.get(process.env.CHANNEL_ID).send(
+    const channel = await bot.channels.cache.get(process.env.CHANNEL_ID);
+
+    channel.send(
       ` GOOD MORNING BULLS
         ***SPY***
         Open: ${latestPrice}
         Previous Close: ${previousClose}
-        YTD Change: ${parseFloat(ytdChange * 100).toFixed(3)}
+        YTD Change: ${parseFloat(ytdChange * 100).toFixed(3)}%
       `
     );
   });
@@ -26,16 +27,18 @@ const scheduleSPYSummaryOpen = async () => {
 
 const scheduleSPYSummaryClose = async () => {
   cron.schedule("0 13 * * Monday-Friday", async () => {
-      const response = await util.tickerHandler("$SPY");
-      const { latestPrice, prevClose, ytdChange } = response;
+      const response = await util.tickerHandler("SPY");
+      const { latestPrice, open, ytdChange } = response;
 
-      bot.channels.cache.get(process.env.CHANNEL_ID).send(
+      const channel = await bot.channels.cache.get(process.env.CHANNEL_ID);
+
+      channel.send(
         `
           TRADING IS CLOSED FOR THE DAY
           ***SPY***
           Closing: ${latestPrice}
-          Open: ${prevClose}
-          YTD Change: ${parseFloat(ytdChange * 100).toFixed(3)}
+          Open: ${open}
+          YTD Change: ${parseFloat(ytdChange * 100).toFixed(3)}%
         `
       );
   });
--- index.js
+++ index.js
@@ -7,8 +7,6 @@ bot.login(TOKEN);
 const util = require("./util/index");
 const cron = require("./cron/cron");
 
-
-
 bot.on("message", async msg => {
   if (msg.content[0].includes("$")) {
     const ticker = util.prepString(msg.content);
@@ -21,20 +19,22 @@ bot.on("message", async msg => {
       changePercent,
       week52High,
       week52Low,
-      extendedPrice,
+      latestVolume,
+      avgTotalVolume,
       companyName,
     } = response;
 
     msg.reply(
-      `
+    `
     ***${symbol}***
     ${companyName}
     Latest Price: ${latestPrice}
     Change Amount: $${change}
     Change Percent: ${parseFloat(changePercent * 100).toFixed(3)}%
+    Volume: ${latestVolume}
+    AvgTotalVolume: ${avgTotalVolume}
     52wk High: ${week52High}
     52wk Low: ${week52Low}
-    After Hours Price: ${extendedPrice || "Not Available"}
     `
     );
   }
