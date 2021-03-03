cebb901bd8b759b95c561b534b5e861078d1fc70
pngo12 <pngo12@gmail.com>
2021-03-03 10:26:54 -0800

    Youtube API

--- youtube/youtube.js
+++ youtube/youtube.js
@@ -1,14 +1,30 @@
 require("dotenv").config();
-const YOUTUBETOKEN = process.env.YOUTUBETOKEN;
-const fetch = require("node-fetch");
+const YOUTUBETOKEN = process.env.YOUTUBE_TOKEN;
+
+const { google } = require("googleapis");
+const youtube = google.youtube({
+  version: "v3",
+  auth: YOUTUBETOKEN,
+});
 
 
-const getYoutubeURL = async query => {
-    const baseURL = "https://youtube.com/watch?v="; //assuming videoID is v= here
-
-    const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search`
-    );
 
 
-}
+const getYoutubeURL = async (query) => {
+  let baseURL = "https://youtube.com/watch?v=";
+
+  const callYouTube = await youtube.search.list({
+    part: "id, snippet",
+    q: "cats",
+  });
+
+
+  if (callYouTube.status !== 200) {
+    return "Error processing your request";
+  } else {
+    baseURL = baseURL + callYouTube.data.items[0].id.videoId;
+    return baseURL
+  }
+};
+
+exports.getYoutubeURL = getYoutubeURL;
