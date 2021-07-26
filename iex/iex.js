634005e72f9fe3023abae42321c627291cbda938
pngo12 <pngo12@gmail.com>
2021-07-26 07:52:50 -0700

    comment out response obj

--- iex/iex.js
+++ iex/iex.js
@@ -10,46 +10,46 @@ const getTickerInfo = async ticker => {
     );
 
     // let quote = new util.quote();
-    const isError = await handleErrors(response.ok, quote);
+    const isError = await handleErrors(response.ok);
 
     if (!response.ok) {
       // Do something
     } else {
       let jsonResponse = await response.json();
-      const {
-        symbol,
-        latestPrice,
-        change,
-        changePercent,
-        week52High,
-        week52Low,
-        latestVolume,
-        avgTotalVolume,
-        companyName,
-      } = jsonResponse;
+      // const {
+      //   symbol,
+      //   latestPrice,
+      //   change,
+      //   changePercent,
+      //   week52High,
+      //   week52Low,
+      //   latestVolume,
+      //   avgTotalVolume,
+      //   companyName,
+      // } = jsonResponse;
 
-      let quote = new util.quote(
-        symbol,
-        latestPrice,
-        change,
-        changePercent,
-        week52High,
-        week52Low,
-        latestVolume,
-        avgTotalVolume,
-        companyName
-      );
+      // let quote = new util.quote(
+      //   symbol,
+      //   latestPrice,
+      //   change,
+      //   changePercent,
+      //   week52High,
+      //   week52Low,
+      //   latestVolume,
+      //   avgTotalVolume,
+      //   companyName
+      // );
 
       return jsonResponse;
     }
   }
 };
 
-const handleErrors = async (response, quote) => {
+const handleErrors = async (response) => {
 
   if (!response) {
-    quote.isError = true;
-    quote.errorMessage = "Error processing your request, please try again"
+    // quote.isError = true;
+    // quote.errorMessage = "Error processing your request, please try again"
   } else {
     return false;
   } 
